a2b5863c18d01d672154e03ae4f151171d40a2ac
myleslouw <56117169+myleslouw@users.noreply.github.com>
2019-10-07 09:31:05 +0200

    changes done to the whole ranged Unit class

--- GADE Task 1/RangedUnit.cs
+++ GADE Task 1/RangedUnit.cs
@@ -6,212 +6,180 @@ using System.Threading.Tasks;
 
 namespace GADE_Task_1
 {
-    class RangedUnit : Unit
+    internal class RangedUnit : Unit
     {
-        public RangedUnit(int x, int y, int health, int speed, int attack, int attackRange, int team, string symbol)
+        #region UNIT ATTRIBUTES
+        public RangedUnit(int x, int y, int health, int speed, int attack, int range, string team, string symbol, bool attacking, string name) : 
+            base(x, y, health, speed, attack, range, team, symbol,attacking, "Archer"); //set parameters for ranged unit and affect base
+        class inheritance
         {
-            this.x = x;
-            this.y = y;
-            this.health = health;
-            this.speed = speed;
-            this.attack = attack;
-            this.attackRange = attackRange;
-            this.team = team;
-            this.symbol = symbol;
+        }
+        #endregion
+
+        #region UNIT ACCESSORS TO BASE CLASS
+
+        protected int Health
+        {
+            get { return base.health; }
+            set { base.health = value; }
         }
 
-        // I used the override keyword to override the abstract method in the Unit class
-        public override void Move(ref Unit closestUnit)
+        protected int Speed
         {
-            //if this melee unit is the only closest unit remaining
-            if (this == closestUnit)
-            {
-                return; //then use return to end the method
-            }
-
-            //only react to enemy units- so if not team
-            if (closestUnit.Team != team)
-            {
-                //combat
-                if (health < 25)   //if the health is below 25hp then they must run away
-                {
-                    Random r = new Random();  //inheritance from class Random produces random sequence of number
-                                              // random.Next method generates a random number between 0 to 2
-
-                    switch (r.Next(0, 2))
-                    {
-                        case 0: x += (1 * speed); break; // this means that x position + 1 times speed, so it will move one and use the speed ( x + 1 X speed)
-                        case 1: x -= (1 * speed); break; //x - 1
-                    }
-
-                    switch (r.Next(0, 2)) //same but this time for y
-                    {
-                        case 0: y += (1 * speed); break;  //speed is the move distance
-                        case 1: y -= (1 * speed); break;
-                    }
-
-                    //checks the bounds and resets character
-
-                    if (x <= 0)
-                    {
-                        x = 0;
-                    }
-                    else if (x >= 20)
-                    {
-                        x = 20;
-                    }
-                    if (y <= 0)
-                    {
-                        y = 0;
-                    }
-                    else if (y >= 20)
-                    {
-                        y = 20;
-                    }
-                }
-
-                //cheacks if in combat
-                //maths.abs returns the absolute value of a number
-                else if (Math.Abs(x - closestUnit.X) <= speed && Math.Abs(y - closestUnit.Y) <= speed)  //the distane between 2 position
-                {
-                    Combat(ref closestUnit);
-                }
-                else //move towards the closest unit
-                {
-                    if (x > closestUnit.X) //if ahead then go backwards
-                    {
-                        x -= speed;
-                    }
-                    else if (x < closestUnit.X)
-                    {
-                        x += speed;
-                    }
-                    if (y > closestUnit.Y)
-                    {
-                        y -= speed;
-                    }
-                    else if (y < closestUnit.Y)
-                    {
-                        y += speed;
-                    }
-                }
-
-            }
-
+            get { return base.speed; }
+            set { base.speed = value; }
         }
 
-        public override void Death()
+        protected int Attack
         {
-            throw new DeathException(this.ToString() + " IS DEAD");  //throw a message as a string using the ToString method
+            get { return base.attack; }
+            set { base.attack = value; }
         }
 
-        public override int Attack
+        protected int Range
         {
-            get
-            {
-                return Attack;
-            }
-            set
-            {
-                Attack = value;
-            }
+            get { return base.range; }
+            set { base.range = value; }
         }
-        public override int X
+
+        protected string Team
         {
-            get
-            {
-                return X;
-            }
-            set
-            {
-                X = value;
-            }
+            get { return base.team; }
+            set { base.team = value; }
         }
-        public override int Y
+
+        protected string Symbol
         {
-            get
-            {
-                return Y;
-            }
-            set
-            {
-                Y = value;
-            }
+            get { return base.symbol; }
+            set { base.symbol = value; }
         }
-        public override string Symbol
+
+        protected bool Attacking
         {
-            get
-            {
-                return Symbol;
-            }
-            set
-            {
-                Symbol = value;
-            }
+            get { return attacking; }
+            set { base.attacking = value; }
         }
-        public override int Team
+
+        #endregion UNIT ACCESSORS TO BASE CLASS
+
+        #region UNIT DAMAGE & COMBAT
+
+        public override void handleCombat(Unit enemy) //handle damage the enemy takes
         {
-            get
-            {
-                return Team;
-            }
-            set
-            {
-                Team = value;
-            }
+            enemy.takeDamage(this.Attack); //referenced object gets damaged
         }
-        public override void Combat(ref Unit attacker)
+
+        public override void damageUnit(int attack) //if the unit attacks another unit it will damages the unit
         {
-            this.health = this.health - attacker.Attack; //melee unit health - attacker health
-            if (health <= 0)
-            {
-                Death();
-            }
+            this.health -= attack;
         }
-        public override bool InRange(ref Unit attacker)
+
+        #endregion UNIT DAMAGE & COMBAT
+
+        #region UNIT DISTANCE, RANGE and POSITION
+
+        public override int getDistance(int enemyX, int enemyY)
+        //Calculate distance between unit positions using absolute object.
         {
-            if (DistanceTo(attacker) == attackRange)
-            {
-                return true;
-            }
-            else
+            int distanceX = Math.Abs(enemyPositionX - x);
+            int distanceY = Math.Abs(enemyPositionY - y);
+
+            double enemyXY = Convert.ToDouble((distanceX * distanceX)
++ (distanceY * distanceY));
+
+            return Convert.ToInt32(Math.Sqrt(enemyXY));
+        }
+
+        public override void moveToPosition(int newX, int newY)
+        {
+            this.X = newX;
+            this.Y = newY;
+        }
+
+        public override bool isWithinRange(int posX, int posY)
+        {
+            return this.getDistance(posX, posY) <= this.Range; //is the unit in range? If so, get the distance and then return it for storage.
+        }
+
+        #endregion UNIT DISTANCE, RANGE and POSITION
+
+        #region UNIT DESTRUCTION
+
+        public override void destroyUnit(Object[,] mapPos)
+        {
+            //Remove unit from map and replace as null/ nothing in place.
+            mapPos[this.X, this.Y] = null;
+        }
+
+        public override bool isDead() //check if the player is dead.
+        {
+            if (this.maxHealth == 0)
             {
                 return false;
             }
-        }
-        private int DistanceTo(Unit unit)
-        {
-            int dx = Math.Abs(unit.X - x);   //compares positions
-            int dy = Math.Abs(unit.Y - y);
-            double part = Convert.ToDouble((dx * dx) + (dy * dy));  //convert value to double  then multiply x and y postions by themselves
-            return Convert.ToInt32(Math.Sqrt(part)); //get square root of part above
+            return (this.Health / this.maxHealth) <= 0.25;
         }
 
-        public override Unit Closest(ref Unit[] map)
+        #endregion UNIT DESTRUCTION
+
+        #region UNIT NEAREST ENEMY
+
+        public override Unit nearestUnit(Object[,] mapPos)
         {
-            Unit closest = this;
-            int smallestRange = 100;
-            foreach (Unit u in map)
+            int minimumDistance = 20;
+
+            int mapBounds = 20; //ranged unit range may not exceed 20
+
+            Unit closestEnemy = null;
+
+            for (int x = 0; x < mapBounds; x++)
             {
-                if (u.Team != team)  //if not units team
+                for (int y = 0; y < mapBounds; y++)
                 {
-                    if (smallestRange > DistanceTo(u) && u != this)  //if less than the smallest range and not the melee unit
+                    //If it detects enemy, pass
+                    Object obj = mapPos[x, y];
+
+                    if (obj is Building)
                     {
-                        smallestRange = DistanceTo(u); //then the smallest range
-                        closest = u; //and the closest unit is u
+                        continue;
+                    }
+
+                    Unit possibleEnemy = (Unit)obj;
+
+                    if (possibleEnemy != null && possibleEnemy.Team != this.team) //still checking if true it is an enemy
+                    {
+                        int distance = this.getDistance(possibleEnemy.X, possibleEnemy.Y); //If confirmed, get the distance of the enemy
+
+                        if (minimumDistance > distance)
+                        {
+                            nearestEnemy = possibleEnemy;
+                            shortestDistance = distance;
+                        }
                     }
                 }
             }
-            return closest;
+
+            return nearestEnemy;
         }
 
+        #endregion UNIT NEAREST ENEMY
 
-        public override string ToString()
+        #region UNIT SHOW STATS REPORT
+
+        public override String toString()
         {
-            return symbol + ": [" + x + "," + y + "] " + health + "hp " + attack;
+            string display =
+                "Name: " + Name + "\n" +
+                "Health: " + Health + "\n" +
+                "Attack: " + Attack + "\n" +
+                "Range: " + Range + "\n" +
+                "Speed: " + Speed + "\n";
+
+            //TODO: Send to RichTextBox in Forms using appendText.
+
+            return display;
         }
+
+        #endregion UNIT SHOW STATS REPORT
     }
-
-
-
 }
